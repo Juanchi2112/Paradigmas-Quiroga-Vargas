08bdc6b321522705002446fabb1929e02287646a
Juanchi2112 <juanchiquir@gmail.com>
2023-08-16 10:01:36 -0300

    J

--- Link.hs
+++ Link.hs
@@ -9,11 +9,13 @@ data Link = Lin City City Quality deriving (Eq, Show)
 newL :: City -> City -> Quality -> Link -- genera un link entre dos ciudades distintas
 newL city1 city2 quality = Lin city1 city2 quality
 
+tupleLink (Lin c1 c2 _) = (c1,c2)
+
 connectsL :: City -> Link -> Bool   -- indica si esta ciudad es parte de este link
 connectsL city (Lin c1 c2 _) = city == c1 || city == c2
 
 linksL :: City -> City -> Link -> Bool -- indica si estas dos ciudades distintas estan conectadas mediante este link
-linksL city1 city2 (Lin c1 c2 _) = (city1 == c1 && city2 = c2) || (city1 == c2 && city2 == c1)
+linksL city1 city2 (Lin c1 c2 _) = (city1 == c1 && city2 == c2) || (city1 == c2 && city2 == c1)
 
 capacityL :: Link -> Int
 capacityL (Lin _ _ quality) = capacityQ quality
--- Tunel.hs
+++ Tunel.hs
@@ -1,15 +1,34 @@
 module Tunel ( Tunel, newT, connectsT, usesT, delayT )
    where
 
+import Link
+
 data Tunel = Tun [Link] deriving (Eq, Show)
 
 newT :: [Link] -> Tunel
-newT linkList = Tun linkList
+newT linkList = if  verificarConexion (tupleList linkList) == [Nothing, Nothing] then Tun [] else Tun linkList
 
-checkT (Tun [linkList]) = 
+tupleList (Tun linkList) = [tupleLink x|x <- linkList]
+
+verificarConexion :: [(String, String)] -> [Maybe String]
+verificarConexion listaTuplas = do
+    let ciudades = foldr (\(ciudad1, ciudad2) acc -> ciudad1 : ciudad2 : acc) [] listaTuplas
+        conexiones = foldr (\(ciudad1, ciudad2) acc -> (ciudad1, ciudad2) : (ciudad2, ciudad1) : acc) [] listaTuplas
+        extremos = [ciudad | ciudad <- ciudades, enlaces ciudad == 1]
+        enlaces ciudad = length $ filter (\(c1, c2) -> c1 == ciudad) conexiones
+    
+    case extremos of
+        [puntoEntrada, puntoSalida] -> [Just puntoEntrada, Just puntoSalida]
+        _ -> [Nothing, Nothing]
 
 connectsT :: City -> City -> Tunel -> Bool -- inidca si este tunel conceta estas dos ciudades distintas
-connectsT city1 city2 (Tun linkList) = 
+connectsT city1 city2 (Tun linkList) = verificarConexion linkList == [Just city1, Just city2] || verificarConexion linkList == [Just city2, Just city1]
+
 
 usesT :: Link -> Tunel -> Bool  -- indica si este tunel atraviesa ese link
+usesT link (Tun linkList) = elem link linkList
+
+
 delayT :: Tunel -> Float -- la demora que sufre una conexion en este tunel
+delayT (Tun []) = 0
+delayT (Tun l:linkList) =  delayL l + delayT (Tun linkList)
